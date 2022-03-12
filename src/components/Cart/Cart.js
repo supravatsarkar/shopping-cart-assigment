76a50347f89e8deeea926638b40a5a263b616eb6
Supravat Sarkar <supravatsarkar18@gmail.com>
2022-03-12 22:01:23 +0530

    [update] cart correncey

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,20 +26,20 @@ const Cart = ({ cartDisplay, setCartDisplay, cart, setCart }) => {
             {/* close botton  */}
             <FaWindowClose onClick={closeButton} id="cartCloseBtn" className='fs-2 m-0 p-0' />
 
-            <h2 className='text-center fs-5'>Your Cart: {cart.length}</h2>
+            <h2 className='text-center fs-5 bold'>Your Cart: {cart.length}</h2>
             <div>
                 {
                     cart?.map((cartProduct, index) => <div key={index}>
                         <div class=" shadow mb-2 p-1 rounded" >
                             <div class="row g-0">
                                 <div class="col-md-2">
-                                    <img src={cartProduct?.img} class="img-fluid rounded-circle  " alt="..." />
+                                    <img src={cartProduct?.img} class="img-fluid  " alt="..." />
                                 </div>
-                                <div class="col-md-6" style={{ fontSize: '14px' }}>
-                                    <p class="text-muted" >{cartProduct?.name?.slice(0, 20)}..</p>
+                                <div class="col-md-6 " style={{ fontSize: '12px' }}>
+                                    <p class="text-muted m-0 " >{cartProduct?.name?.slice(0, 20)}..</p>
                                     Quantity: {cartProduct.quantity}
                                 </div>
-                                <div className='col-md-3'>
+                                <div className='col-md-3' style={{ fontSize: '14px' }}>
                                     <p className='text-primary '>${cartProduct?.price}</p>
 
                                 </div>
@@ -53,9 +53,9 @@ const Cart = ({ cartDisplay, setCartDisplay, cart, setCart }) => {
                 }
             </div>
             <hr />
-            <p className='fs-6 fw-bolder'>Total: {total.toFixed(2)}</p>
+            <p className='fs-6 fw-bolder '>Total: ${total.toFixed(2)}</p>
             {
-                cart.length !== 0 && <div className="btn btn-warning" onClick={() => {
+                cart.length !== 0 && <div className="btn btn-warning btn-sm" onClick={() => {
                     if (cart.length) {
                         alert('WOW! Your order successfully placed');
                         setCart([])
